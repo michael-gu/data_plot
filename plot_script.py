54bc0cd1636e526126a049f2db5ec31147a0e226
Michael Gu <michgu@cs.washington.edu>
2024-02-26 21:05:19 -0800

    update

--- plot_script.py
+++ plot_script.py
@@ -1,7 +1,15 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+import sys
+arg = sys.argv[1]
 
-df = pd.read_csv('resnet18_bm.csv')
+if arg == 'resnet':
+    df = pd.read_csv('resnet18_bm.csv')
+elif arg == 'vit':
+    df = pd.read_csv('vit_bm.csv')
+else:
+    print('Error: Invalid argument')
+    sys.exit()
 
 # Convert all trial times from seconds to minutes
 df.loc[:, df.columns != 'Dataset'] /= 60
@@ -25,18 +33,8 @@ features = [
 colors = ['#b8b8b8', '#1a80bb', '#8cc5e3']
 combined_colors = ['blue', 'orange', 'green', 'red', 'purple', 'brown']
 
-# Define a dictionary that maps each feature to a specific hatch pattern
-hatches = {
-    'None': '/',
-    'Vanilla': '\\',
-    'Multithreaded': '|',
-    'Cryptographic Hash': '-',
-    'Multithreaded Cryptographic Hash': '+',
-    'Multithreaded Cryptographic Hash and Plain Model': 'x'
-}
-
 for i, feature in enumerate(features[:-1], 1):
-    fig, ax = plt.subplots(figsize=(10, 9))
+    fig, ax = plt.subplots(figsize=(6, 5))
     grouped_mean[feature].plot.bar(ax = ax, yerr=grouped_std[feature], capsize=4, color=colors[:len(feature)])
     for j, bar in enumerate(ax.patches):
         feature_index = j // len(grouped_mean.index)
@@ -44,33 +42,39 @@ for i, feature in enumerate(features[:-1], 1):
             none_average_time = grouped_mean['None'].iloc[j % len(grouped_mean.index)]
             feature_average_time = grouped_mean[feature[feature_index]].iloc[j % len(grouped_mean.index)]
             overhead = ((feature_average_time / none_average_time) - 1) * 100
-            ax.text(bar.get_x() + bar.get_width() / 2, bar.get_height() + grouped_std[feature[feature_index]].iloc[j % len(grouped_mean.index)] + 1, f'+{overhead:.2f}%', ha='center', va='bottom', fontsize=8)
+            ax.text(bar.get_x() + bar.get_width() * 1.1, bar.get_height() + grouped_std[feature[feature_index]].iloc[j % len(grouped_mean.index)] + 1, f'+{overhead:.2f}%', ha='center', va='bottom', fontsize=10, rotation=30)
 
-    ncol = 3 if len(feature) > 3 else len(feature)
-    ax.legend(ncol=ncol, bbox_to_anchor=(0.5, 1), loc='lower center')
+    ncol = 2 if len(feature) > 2 else len(feature)
+    ax.legend(ncol=ncol, bbox_to_anchor=(0.5, 1), loc='lower center', fontsize=10)
 
-    plt.xticks(rotation=0)
+    plt.xticks(rotation=0, fontsize=12)
     plt.tick_params(axis='x', length=0)
     ax.set_xlabel('')
-    ax.set_ylabel('Time Elapsed (minutes)')
+    ax.set_ylabel('Time Elapsed (minutes)', fontsize=12)
+    
+    if arg == 'resnet':
+        ax.set_ylim(0, 160)
+    else:
+        ax.set_ylim(0, 3000)
+    
     plt.tight_layout()
     
-    plt.savefig(f'{i}.svg', format='svg')
+    plt.savefig(f'{arg}_{i}.svg', format='svg')
     plt.show()
     
 
-fig, ax = plt.subplots(figsize=(10, 9))
+fig, ax = plt.subplots(figsize=(6, 5))
 feature = features[-1]
 grouped_mean[feature].plot.bar(ax=ax, yerr=grouped_std[feature], capsize=4, color=combined_colors[:len(feature)])
 
-ncol = 3 if len(feature) > 3 else len(feature)
-ax.legend(ncol=ncol, bbox_to_anchor=(0.5, 1), loc='lower center')
+ncol = 2 if len(feature) > 2 else len(feature)
+ax.legend(ncol=ncol, bbox_to_anchor=(0.5, 1), loc='lower center', fontsize=10)
 
-plt.xticks(rotation=0)
+plt.xticks(rotation=0, fontsize=12)
 plt.tick_params(axis='x', length=0)
 ax.set_xlabel('')
-ax.set_ylabel('Time Elapsed (minutes)')
+ax.set_ylabel('Time Elapsed (minutes)', fontsize=12)
 plt.tight_layout()
 
-plt.savefig(f'{i}.svg', format='svg')
+plt.savefig(f'{arg}_6.svg', format='svg')
 plt.show()
